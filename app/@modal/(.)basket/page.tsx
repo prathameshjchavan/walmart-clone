8e2c26d59bbceba80c5ce6b42d6bbe694e81c5c4
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-02-28 20:53:09 +0530

    add basket modal

--- app/@modal/(.)basket/page.tsx
+++ app/@modal/(.)basket/page.tsx
@@ -1,5 +1,45 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import Basket from "@/components/basket";
+
 const BasketInterception = () => {
-  return <div>BasketInterception</div>;
+  const router = useRouter();
+
+  const onDismiss = () => {
+    router.back();
+  };
+
+  return (
+    <Dialog
+      open
+      onOpenChange={(isOpen) => {
+        if (!isOpen) {
+          onDismiss();
+        }
+      }}
+    >
+      <DialogContent className="h-4/5 w-full max-w-3xl overflow-y-auto">
+        <DialogHeader>
+          <DialogTitle>Basket</DialogTitle>
+          <DialogDescription>
+            <p>Contents of your basket</p>
+          </DialogDescription>
+        </DialogHeader>
+
+        <Basket />
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default BasketInterception;
